c441b072d931b50c9d45a6baafd5c56f9af99f78
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-30 14:10:14 +0000

    step 20 conditionals

--- 312_Boolean_Gombos.py
+++ 312_Boolean_Gombos.py
@@ -6,3 +6,21 @@ print(type(input("Enter anything: ")))
 #all of the above print functions will print str
 """
 
+#step 20 conditionals
+"""
+ #x = input("pick a number:")
+
+ #if (x < 10): 
+ #  print("too low")
+ #else: 
+ #  print("too high")
+#issue: x is a strung not an integer or float
+#solution:
+x = input("pick a number:")
+
+if (float(x) < 10): 
+  print("too low")
+else: 
+  print("too high")
+"""
+
