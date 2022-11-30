a810df168006ef503e71f829f637746faa9620f2
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-30 13:49:54 +0000

    step 12

---
+++ 312_DataChange_Gombos.py
@@ -0,0 +1,9 @@
+first_age = input("What is the age of person 1? ")
+#asks for the first_age
+second_age = input("What is the age of person 2?  ")
+#asks for the second age
+
+difference = int(first_age) - int(second_age)
+#sets the two variables to integers and finds the difference between the two ages.
+print("Person 1 is", difference, "years older than person 2.")
+#prints the difference between the ages
