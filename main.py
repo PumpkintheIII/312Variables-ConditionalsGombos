ce69669f1b0d57c0c76a52b8a2b43cef12fe580b
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-30 14:04:28 +0000

    created 312_Boolean_Gombos.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ Max Gombos
 PLTW CSE 3.1.2 Variables & Conditionals
 """
 #This file is to allow me to specify the other Python files that run, Not the actual assignment!
+
 #exec(open("steps1-3Predictions.py").read())
 #exec(open("312_DataType_Gombos.py").read())
 #exec(open("312_InputOutput_Gombos.py").read())
-exec(open("312_DataChange_Gombos.py").read())
+#exec(open("312_DataChange_Gombos.py").read())
+exec(open("312_Boolean_Gombos.py").read())
