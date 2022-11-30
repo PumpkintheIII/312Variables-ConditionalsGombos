cd8a214b3097a5c9a5874ba03acc52914b22c68c
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-30 13:30:24 +0000

    added prediction for step 3

--- steps1-3Predictions.py
+++ steps1-3Predictions.py
@@ -1,9 +1,13 @@
-
+"""
 song = 1 + 2
 song = 3.0
 song = "geese"
 print(song)
 #prediction: will print geese
 #result: print's geese
+"""
 
-
+mystery_value = type("Hello World")
+print(mystery_value)
+#prediction: will print Hello World
+#reslut: prints <class 'str'>
