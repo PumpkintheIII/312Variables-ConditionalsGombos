b8875c8d551d602ded23d3a2cc1c073e0a434197
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-30 13:32:25 +0000

    created 312_DataType_Gombos.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ Max Gombos
 PLTW CSE 3.1.2 Variables & Conditionals
 """
 #This file is to allow me to specify the other Python files that run, Not the actual assignment!
-exec(open("steps1-3Predictions.py").read())
+#exec(open("steps1-3Predictions.py").read())
+exec(open("312_DataType_Gombos.py").read())
