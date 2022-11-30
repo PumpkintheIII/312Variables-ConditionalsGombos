feb39381a6522225cfc4c93d8b20991e0ec0d8ac
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-30 13:38:42 +0000

    created 312_InputOutput_Gombos.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ PLTW CSE 3.1.2 Variables & Conditionals
 """
 #This file is to allow me to specify the other Python files that run, Not the actual assignment!
 #exec(open("steps1-3Predictions.py").read())
-exec(open("312_DataType_Gombos.py").read())
+#exec(open("312_DataType_Gombos.py").read())
+exec(open("312_InputOutput_Gombos.py").read())
