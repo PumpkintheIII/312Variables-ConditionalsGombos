9137b2f4ffea6e9a06ee0aff58ae4bbcc22a2f46
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-30 14:07:24 +0000

    step 19, nesting functions

---
+++ 312_Boolean_Gombos.py
@@ -0,0 +1,8 @@
+#step 19 nesting functions
+"""
+print(type(input("type in a name: "))) 
+print(type(input("type in a number: "))) 
+print(type(input("Enter anything: ")))
+#all of the above print functions will print str
+"""
+
