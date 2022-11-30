50afc2a72940d1873076edb0bd4a5019398fad0d
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-30 13:59:23 +0000

    step 14

--- 312_DataChange_Gombos.py
+++ 312_DataChange_Gombos.py
@@ -1,3 +1,4 @@
+"""
 first_age = input("What is the age of person 1? ")
 #asks for the first_age
 second_age = input("What is the age of person 2?  ")
@@ -7,3 +8,14 @@ difference = int(first_age) - int(second_age)
 #sets the two variables to integers and finds the difference between the two ages.
 print("Person 1 is", difference, "years older than person 2.")
 #prints the difference between the ages
+""" 
+print(int(3.14159))
+#converts pi to an int and printis it
+print(str(8675309))
+#converts number to a string and prints it
+print(bool(0))
+#converts 0 to boolean and prints it
+print(bool(1))
+#converts 1 to boolean and prints it
+print(float(10))
+#converts 10 to float and prints it
