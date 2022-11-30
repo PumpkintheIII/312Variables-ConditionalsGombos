d06df251f843078f478cc1f002c40d43701d0e69
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-30 14:14:13 +0000

    step 24 boolean

--- 312_Boolean_Gombos.py
+++ 312_Boolean_Gombos.py
@@ -24,3 +24,16 @@ else:
   print("too high")
 """
 
+#step 22 boolean
+print(True) 
+#print false
+print(5==5) 
+#checks if numbers are equal
+print(2+6==8) 
+#checks if sum of the two numbers is equal to the latter number
+print(5!=6) 
+#checks if the numbers are unequal
+print(3<4) 
+#checks if one number is less than the other
+print(4>=4)
+#checks if one number is greater than or equal to the other
