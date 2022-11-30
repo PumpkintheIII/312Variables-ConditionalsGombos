38a35222536e24f32d0cbaa19cfb9036b0ae68e2
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-30 13:43:51 +0000

    added the code and prediction for step 10

---
+++ 312_InputOutput_Gombos.py
@@ -0,0 +1,23 @@
+x = input("What is your name? ")
+#asks user for name
+y = input("What is your age? ")
+#asks user for age
+
+print("Hello ", x)
+#prints user's name
+print("You are", y, "years old")
+#prints user's age 
+"""
+Predicted Output:
+What is your name?(input for user's name)
+What is your age?(input for user's age)
+Hello (user's name)
+You are (user's age) years old
+"""
+"""
+Output:
+What is your name? Max
+What is your age? 17
+Hello  Max
+You are 17 years old
+"""
