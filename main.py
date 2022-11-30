b1b25d24e6c88fd0f8aa5325d1354905edf482a3
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-30 13:26:27 +0000

    Created steps1-3Predictions.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ Max Gombos
 PLTW CSE 3.1.2 Variables & Conditionals
 """
 #This file is to allow me to specify the other Python files that run, Not the actual assignment!
+exec(open("steps1-3Predictions.py").read())
