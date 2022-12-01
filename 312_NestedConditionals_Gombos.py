252f36a6c94e43df8072c180229b03a6f840bca9
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-12-01 13:05:46 +0000

    step 25, logical operators

---
+++ 312_NestedConditionals_Gombos.py
@@ -0,0 +1,15 @@
+#step 25 logical operators
+# and operator 
+sleep = 8         #Modify this number to see how it changes the output 
+breakfast = True  #Modify this number to see how it changes the output 
+print (sleep>=8 and breakfast==True) 
+
+# or operator 
+food = "cake"     #Modify this food to see how it changes the output 
+print(food=="cake" or food=="muffins") 
+
+# not operator 
+movie = "like"    #Modify this to say "dislike" to see how it changes the output 
+print (movie!="like")
+#prediction: true, true, false
+#output: true, true, false
