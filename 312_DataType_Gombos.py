15b4e68c937274d090ddf148b616740f450405bc
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-12-01 13:18:32 +0000

    added comments

--- 312_DataType_Gombos.py
+++ 312_DataType_Gombos.py
@@ -1,4 +1,5 @@
 #code to predict:
+#each of these print the data type, not the data
 print(type("1")) 
 print(type(-10)) 
 print(type(False)) 
--- 312_NestedConditionals_Gombos.py
+++ 312_NestedConditionals_Gombos.py
@@ -18,13 +18,13 @@ print (movie!="like")
 #step 29 nested conditionals
 guesses = 3  # Change this number to see how the code output changes. 
 
-if guesses >= 1: 
-  x = int(input("pick a number:")) 
-  if x < 10: 
-    print("too low") 
-  else: 
-    print("too high") 
-else: 
-  print("You have run out of guesses!")
+if guesses >= 1: #checks if you have guesses left
+  x = int(input("pick a number:")) #prompts user to guess a number, and sets the number to the 'x' variable
+  if x < 10: #checks if x is less then 10
+    print("too low") #if number is less then 10, tells user to guess higher
+  else: #checks if x is greater than 10
+    print("too high") #if x is greater then 10, tells user to guess lower
+else: #if user has no guesses left, tells user that they are out of guesses
+  print("You have run out of guesses!") 
 #prediction: will check if the user has enough remaining guesses, if they do it will prompt the user to guess a number.it will then check if the number is higher or lower than the number and respond acordingly 
 #output: ^
