957f9b86f26a905ba5a1bfec67b104f6b1459818
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-12-01 13:14:25 +0000

    step 31, nested conditionals

--- 312_NestedConditionals_Gombos.py
+++ 312_NestedConditionals_Gombos.py
@@ -1,4 +1,5 @@
 #step 25 logical operators
+"""
 # and operator 
 sleep = 8         #Modify this number to see how it changes the output 
 breakfast = True  #Modify this number to see how it changes the output 
@@ -13,3 +14,17 @@ movie = "like"    #Modify this to say "dislike" to see how it changes the output
 print (movie!="like")
 #prediction: true, true, false
 #output: true, true, false
+"""
+#step 29 nested conditionals
+guesses = 3  # Change this number to see how the code output changes. 
+
+if guesses >= 1: 
+  x = int(input("pick a number:")) 
+  if x < 10: 
+    print("too low") 
+  else: 
+    print("too high") 
+else: 
+  print("You have run out of guesses!")
+#prediction: will check if the user has enough remaining guesses, if they do it will prompt the user to guess a number.it will then check if the number is higher or lower than the number and respond acordingly 
+#output: ^
